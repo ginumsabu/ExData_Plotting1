519aa9531b66abccfa7539d72666a1df58049380
ginumsabu <ginumsabu@gmail.com>
2017-08-24 02:58:30 -0400

    Add files via upload

---
+++ plot1.R
@@ -0,0 +1,8 @@
+fileurl<-"https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+download.file(fileurl,destfile="eps.zip")
+unzip("eps.zip")
+epc <- read.table("household_power_consumption.txt", header = TRUE,sep=";")
+epcsub<-subset(epc,Date=="1/2/2007" | Date=="2/2/2007")
+png("plot1.png")
+hist(as.numeric(epcsub$Global_active_power)/500,col="red",main="Global Active Power",xlab="Global Active Power (kilowatts)", ylab="Frequency")
+dev.off()
---
+++ plot2.R
@@ -0,0 +1,11 @@
+fileurl<-"https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+download.file(fileurl,destfile="eps.zip")
+unzip("eps.zip")
+epc <- read.table("household_power_consumption.txt", header = TRUE,sep=";")
+epcsub<-subset(epc,Date=="1/2/2007" | Date=="2/2/2007")
+library(dplyr)
+epcsub<-mutate(epcsub,datetime=as.POSIXct(paste(Date," ",Time),format="%d/%m/%Y %H:%M:%S"))
+png("plot2.png")
+par(mar=c(3,4,4,4))
+plot(epcsub$datetime,as.numeric(epcsub$Global_active_power)/500,type="l",ylab="Global Active Power (kilowatts)")
+dev.off()
